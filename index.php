09f388aa97336ba64f7b3f1f3fcca94eaa25770f
wafula <richw.ogola@gmail.com>
2018-09-25 01:30:15 +0300

    assignment 2 commit

--- index.php
+++ index.php
@@ -1,27 +1,88 @@
 <?php
-//Declare The Constant
-define("greeting", "Hello and welcome") ;
-define("dob", 01) ;
+/**
+ * Created by PhpStorm.
+ * User: wrx
+ * Date: 24/09/2018
+ * Time: 22:18
+ * This demonstrates the use of if, if else, switch and arrays.
+ * There is also a demo on operators and functions
+ */
+//Rental Houses
 
-//Declare The Variables
-$word = "When you ask me my age I will tell you," ;
-$rich = "That I am Forever Young!" ;
-$t = date ('y') ;
-$age = $t-dob ;
-$country = "Kenya" ;
+//declaring variables
+$house = "bedsitter";
+$house1 = "one bedroom";
+$house2 = "Flat";
+$house3 = "apartment";
+$rent = 15000;
+$house_rent = 25000;
+$number = 5.732456;
+$length = 10;
+$width = 10;
+$area = $length * $width;
 
-//Display the values
-echo greeting ;
-echo "<br>" ;
-echo "My name is WRX and I was born in $country in the year ";
-echo dob ;
-echo "<br>" ;
-echo $word.$rich;
-echo "<br> This is Because<br>" ;
-if ($t > dob ) {
-    echo "I will always be: $age" ;
-} else {
-    echo "You are too young for this" ;
+//if statement
+if ($house == "bedsitter");
+echo "Your monthly payment plan will be: $rent";
+echo "<br>";
+
+//switch
+switch ($house_rent)
+{
+    case 15000;
+        echo "this will afford $house";
+        break;
+        echo "<br>";
+    case 20000;
+        echo "This will afford $house1";
+        break;
+        echo "<br>";
+    case 25000;
+        echo "This will afford $house2";
+        break;
+        echo "<br>";
+    case 30000;
+        echo "This will afford $house3";
+        break;
+        echo "<br>";
+    case 35000;
+        echo "This is now your level";
+        break;
+        echo "<br>";
+    default;
+        echo "Otherwise you are searching the wrong places";
+        echo "<br>";
 }
+
+// if else statement
+
+    echo "<br>";
+
+if ($number <= 4.732456)
+  {
+    echo ceil($number) . "<br>";
+  } elseif ($number != 4)
+  {
+      echo floor($number) . "<br>";
+  } else
+  {
+      echo "The trick is in the sequence";
+  }
+
+//comparison of two values
+
+if ($length === $width)
+   {
+       echo "This is a square and I shall not work on it!";
+   } else
+   {
+       echo $area;
+   }
 ?>
+
+
+
+
+
+
 
